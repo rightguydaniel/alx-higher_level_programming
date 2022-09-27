cf6e00135c2224a30c7782e1921b28d52af80bb8
rightguydaniel <rightguydaniel@gmail.com>
2022-09-27 04:17:27 +0000

    commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if not my_list:
-        return
-    new_list = my_list.copy()
-    if 0 <= idx < len(new_list):
+    length = len(my_list)
+
+    new_list = my_list[:]
+
+    if 0 <= idx < length:
         new_list[idx] = element
-    return new_list
+
+    return (new_list)
